0d4bb135e5f005afc6fbe4d8588e6d730a242159
Nameyray <eparis@atlassian.com>
2022-06-01 17:39:53 +0300

    Initial commit

---
+++ app/src/main/java/com/moringaschool/myrestaurants/Constants.java
@@ -0,0 +1,6 @@
+package com.moringaschool.myrestaurants;
+
+public class Constants {
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/";
+    public static final String YELP_API_KEY = BuildConfig.kCs7ccdCons9NQmHK9iQgJusDs1CTXGqYYPkaixGo8pAO57kDFLoBwMoNwej3gwbGm7nw0lJ83Xhixnhbkvh_8ab0vxQhDBDSA3mgglYvJC5UaIFUTthM0b-vjuXYnYx;
+}
